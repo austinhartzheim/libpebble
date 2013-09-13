50c9e9ac8891e64dfb7941dbd643c9ef69429fbc
Meiguro <meiguro@meiguro.com>
2013-09-13 01:36:11 -0700

    Improve the watchapp template in PblProjectCreator

--- pebble/PblProjectCreator.py
+++ pebble/PblProjectCreator.py
@@ -84,48 +84,60 @@ FILE_DUMMY_MAIN = """#include <pebble_os.h>
 static Window *window;
 static TextLayer *text_layer;
 
-void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
 }
 
-void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Up");
 }
 
-void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Down");
 }
 
-void config_provider(ClickConfig **config, Window *window) {
+static void click_config_provider(ClickConfig **config, void *context) {
   config[BUTTON_ID_SELECT]->click.handler = select_click_handler;
   config[BUTTON_ID_UP]->click.handler = up_click_handler;
   config[BUTTON_ID_DOWN]->click.handler = down_click_handler;
 }
 
-void handle_init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, (ClickConfigProvider) config_provider);
-  window_stack_push(window, true /* Animated */);
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create(GRect(/* x: */ 0, /* y: */ 74,
-                                       /* width: */ 144, /* height: */ 20));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
   text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
-void handle_deinit(void) {
+static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit(void) {
   window_destroy(window);
 }
 
 int main(void) {
-  handle_init();
+  init();
 
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
   app_event_loop();
-  handle_deinit();
+  deinit();
 }
 """
 
