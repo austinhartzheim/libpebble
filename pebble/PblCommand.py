dbeaefca7643edd67ea9990c1f665f0ecc5b34d0
Steve Thompson <sthompson@getpebble.com>
2014-02-12 19:33:38 -0800

    Allow SDK location to be overridden by environment variable.

--- pebble/PblCommand.py
+++ pebble/PblCommand.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 class PblCommand:
     name = ''
@@ -9,15 +10,20 @@ class PblCommand:
 
     def configure_subparser(self, parser):
         parser.add_argument('--sdk', help='Path to Pebble SDK (ie: ~/pebble-dev/PebbleSDK-2.X/)')
-        parser.add_argument('--debug', action='store_true', 
+        parser.add_argument('--debug', action='store_true',
                 help = 'Enable debugging output')
 
     def sdk_path(self, args):
         """
         Tries to guess the location of the Pebble SDK
         """
-
+        sdk_path = os.getenv('PEBBLE_SDK_PATH')
         if args.sdk:
             return args.sdk
+        elif sdk_path:
+            if not os.path.exists(sdk_path):
+                raise Exception("SDK path {} doesn't exist!".format(sdk_path))
+            logging.info("Overriding Pebble SDK Path with '%s'", sdk_path)
+            return sdk_path
         else:
             return os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '..'))
