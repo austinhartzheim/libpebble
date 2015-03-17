9112680eed75918744561dfe618b692c38a69417
Kirby Kohlmorgen <kirby@purdue.edu>
2015-03-17 18:44:12 -0400

    Remove duplicate code

--- pebble/LibPebblesCommand.py
+++ pebble/LibPebblesCommand.py
@@ -70,9 +70,6 @@ class LibPebbleCommand(PblCommand):
         if not args.phone and not args.pebble_id and not args.emulator and not args.qemu:
             args.emulator = 'basalt'
 
-        auth_storage_file = os.path.join(self.get_persistent_dir(), 'oauth_storage')
-        account = get_default_account(auth_storage_file)
-
         num_args = bool(args.phone) + bool(args.pebble_id) + bool(args.qemu) + bool(args.emulator)
         if num_args > 1:
             raise ConfigurationException("You must specify only one method to connect to the watch "
